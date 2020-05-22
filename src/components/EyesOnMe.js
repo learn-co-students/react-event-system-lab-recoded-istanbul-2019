b14d2da5f7980ca58818586ed9e0020c4c51bbcb
basil-kawak <basilalisa0@gmail.com>
2020-05-22 19:32:48 +0300

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
 // Code EyesOnMe Component Here
+import React, { Component } from 'react';
+export default class EyesOnMe extends Component {
+
+
+    onFocusHandeler = () => {
+        console.log('Good!')
+    }
+
+    onBlurHandeler = () => {
+        console.log('Hey! Eyes on me!')
+    }
+
+    render() {
+        return (
+            <button onFocus={this.onFocusHandeler} onBlur={this.onBlurHandeler}></button>
+        )
+    }
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,15 @@
 // Code Keypad Component Here
+import React, { Component } from 'react';
+export default class Keypad extends Component {
+
+
+    keyUpHandelr = () => {
+        console.log('Entering password...')
+    }
+
+    render() {
+        return (
+            <input onKeyUp={this.keyUpHandelr} type="password"></input>
+        )
+    }
+}
