8c11787155bad8844e0a2b078a309a23746b4de3
emrerdem1 <emrerdem94@gmail.com>
2020-05-26 02:54:20 +0300

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,14 @@
-// Code EyesOnMe Component Here
+import React from 'react';
+
+export default class EyesOnMe extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    blurIT = () => console.log('Hey! Eyes on me!');
+    focusIT = () => console.log('Good!');
+    render() {
+        return (
+            <button onBlur={this.blurIT} onFocus={this.focusIT}></button>
+        )
+    }
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,10 @@
-// Code Keypad Component Here
+import React from 'react';
+
+export default class Keypad extends React.Component {
+    listenPassword = () => console.log('Entering password...');
+    render() {
+        return (
+            <input type="password" name="pass" onKeyUp={this.listenPassword}></input>
+        )
+    }
+}
